ef685cff9090b9d6f028958101ee3ef41e27369d
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2021-07-17 08:50:53 -0300

    commit inicial, começamos a fazer o dashboard do feedbacker no curso

---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
+  </div>
+</template>
+
+<script>
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    const handleLogin = () => {
+
+    }
+    const handleAccountCreate = () => {
+
+    }
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>
